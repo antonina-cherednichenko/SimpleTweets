4f25d443db7f999f03e8cca2f103d83880793a46
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2017-03-24 15:31:52 -0700

    change .gitignore

--- app/src/main/java/com/codepath/apps/simpletweets/db/TwitterDatabase.java
+++ app/src/main/java/com/codepath/apps/simpletweets/db/TwitterDatabase.java
@@ -1,9 +1,9 @@
-package com.codepath.apps.simpletweets;
+package com.codepath.apps.simpletweets.db;
 
 import com.raizlabs.android.dbflow.annotation.Database;
 
-@Database(name = MyDatabase.NAME, version = MyDatabase.VERSION)
-public class MyDatabase {
+@Database(name = TwitterDatabase.NAME, version = TwitterDatabase.VERSION)
+public class TwitterDatabase {
 
     public static final String NAME = "RestClientDatabase";
 
